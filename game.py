21e2b9864edf5431a5cae6a3eb398491c3835a49
mathisdlg <mathis.delage@nordnet.fr>
2022-10-27 11:01:05 +0200

    Stat point + method win level + method win stat point

--- game.py
+++ game.py
@@ -12,6 +12,7 @@ class Player:
         - Classe
         - Race
         - Level
+        - Point de statistiques
         - Statistics:
             - PV
             - PM
@@ -27,6 +28,7 @@ class Player:
         self.classe = classe
         self.race = race
         self.level = 0
+        self.stat_point = 0
         self.stats = {"PV": 100, "PM": 100, "STR": 1, "AG": 1, "INT": 1, "WIS": 1, "LUK": 1}
     
     def __str__(self) -> str:
@@ -34,6 +36,12 @@ class Player:
     
     def __format__(self, __format_spec: str) -> str:
         return self.name
+    
+    def win_level(self, nb):
+        self.level +=nb
+    
+    def win_stat_point(self, nb):
+        self.stat_point += nb
     
     def say_hello(self, p_hello):
         print("Hello {}".format(p_hello))
