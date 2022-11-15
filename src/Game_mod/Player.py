04a03c63f93705dfcfb34cbb42518d5c18f8105b
mathisdlg <mathis.delage@nordnet.fr>
2022-11-15 11:24:45 +0100

    Add new function and import all branch of the module

--- src/Game_mod/Player.py
+++ src/Game_mod/Player.py
@@ -1,3 +1,5 @@
+from Game_mod import *
+
 class Player:
     """
     Player:
@@ -16,13 +18,13 @@ class Player:
             - Luck
 
     """
-    def __init__(self, p_name, classe, race):
+    def __init__(self, p_name, classe: Class, race: Race):
         self.name = p_name
         self.classe = classe
         self.race = race
         self.level = 0
         self.stat_point = 0
-        self.stats = {"PV": 100, "PM": 100, "STR": 1, "AG": 1, "INT": 1, "WIS": 1, "LUK": 1}
+        self.stats = {"PV": 100, "PM": 10, "STR": 1, "AG": 1, "INT": 1, "WIS": 1, "LUK": 1}
     
     def __str__(self) -> str:
         return self.name
@@ -30,12 +32,18 @@ class Player:
     def __format__(self, __format_spec: str) -> str:
         return self.name
     
-    def win_level(self, nb):
+    def win_level(self, nb: int):
         self.level +=nb
     
-    def win_stat_point(self, nb):
+    def win_stat_point(self, nb: int):
         self.stat_point += nb
     
-    def say_hello(self, p_hello):
+    def say_hello(self, p_hello: Player):
         print("Hello {}".format(p_hello))
         return 0
+    
+    def save_player(self):
+        pass
+    
+    def load_player(self):
+        pass
