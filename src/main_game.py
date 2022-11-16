ecded871bbb19fcdea76879e836aef21c9946cb7
mathisdlg <mathis.delage@nordnet.fr>
2022-11-16 16:45:00 +0100

    Test programm for the Game mod

---
+++ src/main_game.py
@@ -0,0 +1,12 @@
+from Game import Player
+from Game import Class
+from Game import Race as R
+
+def test():
+    print(Class.CLASS_LIST)
+    print(R.RACE_LIST)
+    pass
+
+
+if __name__ == "__main__":
+    test()
