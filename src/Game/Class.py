be7972164d1921b44fef4abe9c8e78cc0ec29dcf
mathisdlg <mathis.delage@nordnet.fr>
2022-11-23 11:31:08 +0100

    Add choose class and race method

--- src/Game/Class.py
+++ src/Game/Class.py
@@ -1,5 +1,13 @@
 CLASS_LIST = ["Thief", "Priest", "Mage", "Figther", "Paladin", "Ranger", "Druid", "Black Mage", "Labyrinths Master", "Berserker", "Assasin", "Bard", "Cleric", "Hunter", "Villager", "Beast Master", "Artificer", "Craftsman", "Alchemist", "Wizard"]
 
+
+def choose_class():
+    while True:
+        class_ = input("Enter your class:")
+        if class_ in CLASS_LIST:
+            return class_
+
+
 class Class:
     """
     Class:
--- src/Game/Race.py
+++ src/Game/Race.py
@@ -1,5 +1,12 @@
 RACE_LIST = ["Humain", "Elve", "Dwarf", "Vampire", "Fairy", "Leprechaun", "Orc", "Halfelin", "Dragon", "Robot", "Cyborg"]
 
+def choose_race():
+    while True:
+        race_ = input("Enter your race: ")
+        if race_ in RACE_LIST:
+            return race_
+
+
 class Race:
     """
     Race:
