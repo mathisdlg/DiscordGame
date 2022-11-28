2e392a3026ddb55dee14986aaa12cea157cb8e06
mathisdlg <mathis.delage@nordnet.fr>
2022-11-28 16:01:38 +0100

    Add Doc string + Remove of player import + modify of save function

--- src/Game/Database.py
+++ src/Game/Database.py
@@ -1,23 +1,29 @@
 from os.path import exists
-from Game.Player import Player
 import pickle
 
 
 class GameDB:
 
     def __init__(self):
+        """
+        Database constructor game's database of ##Game name##
+        """
         pass
     
     def connect(self, file_name):
         if exists(file_name):
             self.pick = open(file_name, "r+b")
+            return 0
         else:
             self.pick = open(file_name, "x+b")
             self.save([])
+            return 1
     
     def save(self, object):
         self.pick.seek(0)
         pickle.dump(object, self.pick)
+        self.pick.truncate()
+        return 0
     
     def load(self):
         self.pick.seek(0)
