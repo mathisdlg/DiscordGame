f9dd9ec4003cb6817bed7e9a6dfdfa0f1e7e90e0
mathisdlg <mathis.delage@nordnet.fr>
2022-11-29 13:54:11 +0100

    Get class name function in the class class file

--- src/Game/Class.py
+++ src/Game/Class.py
@@ -38,3 +38,9 @@ class Class:
 
     def __init__(self, class_name):
         self.class_name = class_name
+
+    ########################################################
+    #                   Get method                         #
+    def get_class_name(self):
+        return self.class_name
+    ########################################################
