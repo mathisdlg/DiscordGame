5740e17bcc9c3d4a3b8dbb74e3a5badc8075408e
mathisdlg <mathis.delage@nordnet.fr>
2022-12-23 19:45:01 +0100

    Add 3 mind map to clarify what I am thinking

---
+++ structuration/Statistics.mm
@@ -0,0 +1,45 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1671649574643" ID="ID_736496185" MODIFIED="1671651464590" TEXT="Statistics">
+<edge STYLE="bezier" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1671651467901" ID="ID_760394518" LINK="Player.mm" MODIFIED="1671651501699" POSITION="right" TEXT="Player"/>
+<node COLOR="#79d912" CREATED="1671649591590" ID="ID_24529718" MODIFIED="1671650131393" POSITION="right" TEXT="HP">
+<edge STYLE="bezier" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#12b5d9" CREATED="1671649591591" ID="ID_603311357" MODIFIED="1671650131393" POSITION="right" TEXT="MP">
+<edge STYLE="bezier" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#d91212" CREATED="1671649591591" ID="ID_1673482022" MODIFIED="1671650131393" POSITION="right" TEXT="Strength">
+<edge STYLE="bezier" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#1224d9" CREATED="1671649591592" ID="ID_1326423319" MODIFIED="1671650131393" POSITION="right" TEXT="Defense">
+<edge STYLE="bezier" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#d99112" CREATED="1671649591592" ID="ID_237581448" MODIFIED="1671650131393" POSITION="right" TEXT="Agility">
+<edge STYLE="bezier" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#32901d" CREATED="1671649599230" ID="ID_878935388" MODIFIED="1671650131393" POSITION="left" TEXT="Luck">
+<edge STYLE="bezier" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#90541d" CREATED="1671649599231" ID="ID_617971139" MODIFIED="1671650131393" POSITION="left" TEXT="Wisdom">
+<edge STYLE="bezier" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#6e2ec1" CREATED="1671649599232" ID="ID_843204525" MODIFIED="1671650131393" POSITION="left" TEXT="Intelligence">
+<edge STYLE="bezier" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#ff00ff" CREATED="1671649599232" ID="ID_677992349" MODIFIED="1671650131393" POSITION="left" TEXT="Dodge">
+<edge STYLE="bezier" WIDTH="2"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#c3c666" CREATED="1671718609263" ID="ID_1425370404" MODIFIED="1671737863315" POSITION="left" TEXT="Speed"/>
+</node>
+</map>
