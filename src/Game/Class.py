66ea758ba6f129a021bb66b9d68328d6e4806008
mathisdlg <mathis.delage@nordnet.fr>
2022-12-31 17:47:39 +0100

    Add clarity in the output

--- src/Game/Class.py
+++ src/Game/Class.py
@@ -1,11 +1,20 @@
 CLASS_LIST = ['Thief', 'Priest', 'Mage', 'Figther', 'Paladin', 'Ranger', 'Druid', 'Black Mage', 'Labyrinths Master', 'Berserker', 'Assassin', 'Bard', 'Cleric', 'Hunter', 'Villager', 'Beast Master', 'Artificer', 'Craftsman', 'Alchemist', 'Wizard', 'Bettor', 'Dragon Slayer', 'Blacksmith', 'Necromancer', 'Warlock', 'Elementalist', 'Ninja', 'Monk', 'Crusader', 'Demolitionist']
 
 
+def display_classes():
+    for classe in CLASS_LIST:
+        print(f"{classe}")
+
 def choose_class():
     while True:
-        class_ = input("Enter your class: ")
+        class_ = input("Enter your class[h to see all classes]: ")
         if class_ in CLASS_LIST:
             return class_
+        elif class_ == 'h':
+            display_classes()
+        else:
+            print("Invalid class: {} not found!".format(class_))
+
 
 
 class Class:
--- src/Game/Race.py
+++ src/Game/Race.py
@@ -1,10 +1,20 @@
 RACE_LIST = ['Human', 'Elf', 'Dwarf', 'Vampire', 'Fairy', 'Leprechaun', 'Orc', 'Halfelin', 'Dragon', 'Robot', 'Cyborg', 'Zombie', 'Cyclops', 'Giant', 'Black Elf', 'Half-Demon', 'Troll', 'Metamorph']
 
+
+def display_races():
+    for race in RACE_LIST:
+        print(f"{race}")
+
 def choose_race():
     while True:
-        race_ = input("Enter your race: ")
+        race_ = input("Enter your race[h to see all races]: ")
         if race_ in RACE_LIST:
             return race_
+        elif race_ == 'h':
+            display_races()
+        else:
+            print("Invalid race: {} not found!".format(race_))
+
 
 
 class Race:
